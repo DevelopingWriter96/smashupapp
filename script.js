4363fe9797e919cb982ecc576e1471b058da5532
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-11-15 11:29:02 -0700

    Successfully rendering list

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let factions = ["Ancient Egyptians", "Dinosaurs", "Mad Scientists", "Wizards"]
-console.log(factions)
+let smashlist = document.getElementById("smashlist")
 
-for
+let factions = ["Ancient Egyptians", "Dinosaurs", "Mad Scientists", "Wizards"]
+
+factions.forEach(function(faction) {
+    console.log(faction);
+    let newFaction = document.createElement('li')
+    newFaction.textContent = faction
+    smashlist.appendChild(newFaction)
+})
